0d9ba0c56a264e85f064c1ff308e843079213a88
haewoni <suzyhw96@gmail.com>
2020-09-09 02:54:08 +0900

    .

--- guest_model1.5/WebContent/WEB-INF/views/guest_modify_action.jsp
+++
@@ -1,9 +0,0 @@
-<%@page import="com.itwill.guest.Guest"%>
-<%@page import="com.itwill.guest.GuestService"%>
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
-<%
-	GuestService guestService = (GuestService)request.getAttribute("guestService");
-
-%>
-
--- guest_model2/src/com/itwill/guest/controller/DispatcherServlet.java
+++ guest_model2/src/com/itwill/guest/controller/DispatcherServlet.java
@@ -1,6 +1,7 @@
 package com.itwill.guest.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletConfig;
@@ -10,8 +11,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.itwill.guest.Guest;
+import com.itwill.guest.GuestService;
+
 /*
- * 모든 요청의 진입점(단일 Controller)
+모든요청의 진입점(단일 Controller)
+ 	<<요청command>>
+	/guest_main.do
+	/guest_write_form.do
+	/guest_write_action.do
+	/guest_list.do
+	/guest_view.do
+	/guest_modify_form.do
+	/guest_modify_action.do
+	/guest_remove_action.do
+	/guest_error.do
  */
 
 public class DispatcherServlet extends HttpServlet {
@@ -53,8 +67,134 @@ public class DispatcherServlet extends HttpServlet {
 			forwardPath="forward:/WEB-INF/views/guest_write_form.jsp";	
 			
 		}else if(command.equals("/guest_list.do")) {
+			try {
+				GuestService guestService=new GuestService();
+				ArrayList<Guest> guestList=guestService.selectAll();
+				request.setAttribute("guestList", guestList);
+				forwardPath="forward:/WEB-INF/views/guest_list.jsp";
+			}catch (Exception e) {
+				e.printStackTrace();
+				forwardPath="forward:/WEB-INF/views/guest_error.jsp";
+			}
+			
 			
 		}else if(command.equals("/guest_view.do")) {
+			String guest_noStr = request.getParameter("guest_no");
+//			if(guest_noStr==null||guest_noStr.equals("")){
+////				response.sendRedirect("guest_list.jsp");
+//				forwardPath="redirect:guest_list.do";
+//			}else {
+				try {
+					GuestService guestService=new GuestService();
+					Guest guest=
+							guestService.selectByNo(Integer.parseInt(guest_noStr));
+					request.setAttribute("Guest", guest);
+					forwardPath = "forward:/WEB-INF/views/guest_view.jsp";
+				}catch (Exception e) {
+					e.printStackTrace();
+					forwardPath = "forward:/WEB-INF/views/guest_error.jsp";
+				}
+				
+				
+		}else if(command.equals("/guest_write_action.do")) {
+			if(request.getMethod().equalsIgnoreCase("GET")){
+				//response.sendRedirect("guest_write_form.do");
+				forwardPath="redirect:guest_write_form.do";
+			}else {
+				try{
+					request.setCharacterEncoding("UTF-8");
+					String guest_name=request.getParameter("guest_name");
+					String guest_email=request.getParameter("guest_email");
+					String guest_homepage=request.getParameter("guest_homepage");
+					String guest_title=request.getParameter("guest_title");
+					String guest_content=request.getParameter("guest_content");
+					Guest guest=
+							new Guest(-999,guest_name,
+									  null,guest_email,
+									  guest_homepage,guest_title,
+									  guest_content);
+					GuestService guestService=new GuestService();
+					int insertRowCount=
+							guestService.insertGuest(guest);
+					//response.sendRedirect("guest_list.do");
+					forwardPath="forward:/WEB-INF/views/guest_list.do";				
+					
+				}catch(Exception e){
+					e.printStackTrace();
+//					response.sendRedirect("guest_error.jsp");
+					forwardPath="forward:/WEB-INF/views/guest_error.jsp";
+				}
+			}	
+					
+			
+		}else if(command.equals("/guest_modify_form.do")) {
+			if(request.getMethod().equalsIgnoreCase("GET")){
+				forwardPath="redirect:guest_main.do";
+			}else {
+				try {
+					String guest_noStr = request.getParameter("guest_no");
+					GuestService guestService=new GuestService();
+					Guest guest=
+							guestService.selectByNo(Integer.parseInt(guest_noStr));
+					request.setAttribute("guest", guest);
+					forwardPath="forward:/WEB-INF/views/guest_modify_form.jsp";
+				}catch (Exception e) {
+					e.printStackTrace();
+					forwardPath="forward:/WEB-INF/views/guest_error.jsp";
+				}
+			}
+			
+			
+		}else if(command.equals("/guest_modify_action.do")) {
+//			if(request.getMethod().equalsIgnoreCase("GET")){
+//				forwardPath="redirect:guest_main.do";
+//			}else {
+				try{
+					request.setCharacterEncoding("UTF-8");
+					String guest_noStr=request.getParameter("guest_no");
+					String guest_name=request.getParameter("guest_name");
+					String guest_email=request.getParameter("guest_email");
+					String guest_homepage=request.getParameter("guest_homepage");
+					String guest_title=request.getParameter("guest_title");
+					String guest_content=request.getParameter("guest_content");
+					GuestService guestService=new GuestService();
+					int updateRowCount=
+							guestService.updateGuest(
+										new Guest(Integer.parseInt(guest_noStr),
+											guest_name,null,
+											guest_email,guest_homepage,
+											guest_title,guest_content));
+					forwardPath="redirect:guest_view.do?guest_no="+guest_noStr;
+				}catch(Exception e){
+					e.printStackTrace();
+					forwardPath="forward:/WEB-INF/views/guest_error.jsp";
+					
+				
+			}	
+			
+			
+		}else if(command.equals("/guest_remove_action.do")) {
+			forwardPath="forward:/WEB-INF/views/guest_remove_action.jsp";
+			if(request.getMethod().equalsIgnoreCase("GET")){
+				//response.sendRedirect("guest_main.do");
+				forwardPath="redirect:guest_main.do";
+			}else {
+			    try{
+				    String guest_noStr = request.getParameter("guest_no");
+				    GuestService guestService=new GuestService();
+				    guestService.deleteGuest(Integer.parseInt(guest_noStr));
+				    //response.sendRedirect("guest_list.do");
+				    forwardPath="redirect:guest_list.do";
+			    }catch(Exception e){
+			    	e.printStackTrace();
+			    	//response.sendRedirect("guest_error.do");
+			    	forwardPath="redirect:guest_error.do";
+			    }
+			}	
+			
+			
+		}else if(command.equals("/guest_error.do")) {
+			forwardPath="forward:/WEB-INF/views/guest_error.jsp";
 		
 		}
 		/*
